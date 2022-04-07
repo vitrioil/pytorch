844f7bf7b0f03808ebead57e1db1d758c2b4975c
Peter Bell <peterbell10@live.co.uk>
2022-04-07 16:51:04 +0100

    std/var: Deprecate default correction value
    ghstack-source-id: 7d53a553add1cd255a114842b39e0172d70e54a9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/55679

--- torch/_torch_docs.py
+++ torch/_torch_docs.py
@@ -9252,6 +9252,7 @@ Args:
 Keyword args:
     correction (int): difference between the sample size and sample degrees of freedom.
                       Defaults to `Bessel's correction`_, ``correction=1``.
+      .. warning:: The default correction value is deprecated. Pass ``correction=1`` explicitly instead.
     {keepdim}
     {out}
 
@@ -9306,6 +9307,7 @@ Args:
 Keyword args:
     correction (int): difference between the sample size and sample degrees of freedom.
                       Defaults to `Bessel's correction`_, ``correction=1``.
+      .. warning:: The default correction value is deprecated. Pass ``correction=1`` explicitly instead.
     {keepdim}
     {out}
 
@@ -10582,6 +10584,7 @@ Args:
 Keyword args:
     correction (int): difference between the sample size and sample degrees of freedom.
                       Defaults to `Bessel's correction`_, ``correction=1``.
+      .. warning:: The default correction value is deprecated. Pass ``correction=1`` explicitly instead.
     {keepdim}
     {out}
 
@@ -10635,6 +10638,7 @@ Args:
 Keyword args:
     correction (int): difference between the sample size and sample degrees of freedom.
                       Defaults to `Bessel's correction`_, ``correction=1``.
+      .. warning:: The default correction value is deprecated. Pass ``correction=1`` explicitly instead.
     {keepdim}
     {out}
 
